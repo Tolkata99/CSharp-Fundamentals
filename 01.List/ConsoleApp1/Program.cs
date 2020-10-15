238c3ac283cee21b43de96b4f5d29f78233e6e7c
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-15 23:12:15 +0300

    10.15.20

---
+++ 01.List/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            numbers.Add(500);
+            numbers.Add(555);
+            numbers.Add(777);
+
+            numbers.Remove(4);
+
+            numbers.Insert(2, 997979797);
+            numbers.Sort(2, 77, 2 == 3);
+
+
+            bool isEResult = numbers.Contains(777);
+
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine(isEResult);
+        }
+    }
+}
---
+++ 01.List/ListOfComands/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfComands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+
+
+            while (true)
+            {
+                string[] comand = Console.ReadLine()
+                    .ToUpper()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (comand[0] == "END")
+                {
+                    break;
+                }
+
+
+                if (comand[0] == "ADD")
+                {
+                    numbers.Add(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "REMOVE")
+                {
+                    numbers.Remove(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "REMOVEAT")
+                {
+                    numbers.RemoveAt(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "INSERT")
+                {
+                    numbers.Insert(int.Parse(comand[2]), (int.Parse(comand[1])));
+                }
+                
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
