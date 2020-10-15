c387bc5bdbe827767feab11d2d53f3579b420112
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-15 23:12:46 +0300

    10.15.20

---
+++ 01.List/03.Murch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Murch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine()
+                                     .Split()
+                                     .Select(int.Parse)
+                                     .ToList();
+            List<int> seconds = Console.ReadLine()
+                                       .Split()
+                                       .Select(int.Parse)
+                                       .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < Math.Min(first.Count, seconds.Count); i++)
+            {
+                result.Add(first[i]);
+                result.Add(seconds[i]);
+
+            }
+            for (int i = Math.Min(first.Count, seconds.Count);
+                i < Math.Max(first.Count, seconds.Count); i++)
+            {
+                if(i >= first.Count)
+                {
+                    result.Add(seconds[i]);
+                }
+               else
+                {
+                    result.Add(first[i]);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ 01.List/03Concaten/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03Concaten
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            List<int> numbersSum = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxIndex = numbersSum.Count  / 2;
+
+            for (int i = 0; i < maxIndex; i++)
+            {
+                numbersSum[i] += numbersSum[numbersSum.Count - 1];
+                numbersSum.Remove(numbersSum[numbersSum.Count - 1]);
+            }
+            Console.Write(string.Join(" ", numbersSum));
+
+
+
+
+
+
+        }
+    }
+}
