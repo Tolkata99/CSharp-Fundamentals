9da14df44009fb113a9e3dc32903ddf18ac6c43e
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-15 23:14:52 +0300

    f

--- 01.List/01.List/Program.cs
+++ 01.List/01.List/Program.cs
@@ -8,22 +8,9 @@ namespace _01.List
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine()
-                                       .Split()
-                                       .Select(int.Parse)
-                                       .ToList();
+            string n = Console.ReadLine();
 
-            for (int i = 0; i < numbers.Count - 1; i++)
-            {
-                if (numbers[i] == numbers[i + 1])
-                {
-                    numbers[i] += numbers[i + 1];
-                    numbers.RemoveAt(i + 1);
-                    i += -1;
-                }
-            }
-
-            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine(n);
         }
     }
 }
