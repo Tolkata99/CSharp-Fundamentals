c1129a0bca2c165ef9c3aab9efc89b02ca62d16a
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-15 23:14:08 +0300

    10.15.20

---
+++ 01.List/04.SortStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SortStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>(n);
+            for (int i = 0; i < n; i++)
+            {
+                
+
+                string currProduct = Console.ReadLine();
+                names.Add(currProduct);
+            }
+            names.Sort();
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{names[i]}");
+            }
+        }
+    }
+}
---
+++ 01.List/05.NegativeNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.NegativeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                       .Split(" ")
+                                       .Select(int.Parse)
+                                       .ToList();
+            List<int> result = numbers
+                .Where(n => n >= 0)
+                .Reverse()
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", result));
+
+            
+
+        }
+    }
+}
