af63d547e8d20f309eb8febce6f90c541fe58d93
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-18 18:04:16 +0300

    4

---
+++ 04.ListOperations/04.ListOperations/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                List<string> comand = Console.ReadLine()
+                    .ToUpper()
+                    .Split()
+                    .ToList();
+
+                
+                if(comand[0] == "END")
+                {
+                    break;
+                }
+                else if(comand[0] == "ADD")
+                {
+                    numbers.Add(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "INSERT")
+                {
+                    int index = int.Parse(comand[2]);
+                    int number = int.Parse(comand[1]);
+                                 //Index
+                    if(IsValidIndex(index, numbers.Count))
+                    {
+                        Console.WriteLine("Invalid index!");
+                    }
+                    else
+                    {
+                        numbers.Insert(index, number);
+                    }
+
+                }
+                else if (comand[0] == "REMOVE")
+                {
+                    int index = (int.Parse(comand[1]));
+                    if (IsValidIndex(index, numbers.Count))
+                    {
+                        Console.WriteLine("Invalid index!");
+                    }
+                    else
+                    {
+                        numbers.RemoveAt(index);
+                    }
+                     
+                    
+                }
+                else if (comand[0] == "SHIFT" )
+                {
+                    int rotation = (int.Parse(comand[2]));
+                    if (comand[1] == "RIGHT")
+                    {
+                        for (int i = 0; i < rotation; i++)
+                        {
+                            int firstListNumber = numbers[0];
+                            for (int j = 0; j < numbers.Count - 1; j++)
+                            {
+                                numbers[j] = numbers[j + 1];
+                            }
+                            numbers[numbers.Count - 1] = firstListNumber;
+                        }
+                    }
+                    else if(comand[1] == "LEFT")
+                    {
+
+                        for (int i = 0; i < rotation; i++)
+                        {
+                            int lastNumber = numbers[numbers.Count - 1];
+                            for (int j = numbers.Count - 1; j > 0; j--)
+                            {
+                                numbers[j] = numbers[j - 1];
+
+                            }
+                            numbers[0] = lastNumber;
+                        }
+                    }
+                    
+                }
+                
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        
+
+        public static bool IsValidIndex(int index, int count)
+        {
+            return index > count || index < 0;
+        }
+
+    }
+}
---
+++ 04.ListOperations/bomGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bomGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bombProb = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int bobm = bombProb[0];
+            int power = bombProb[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumbers = numbers[i];
+                if(currentNumbers == bobm)
+                {
+                    int startIndex = i - power;
+                    int endIndex = i + power;
+
+                    if(startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if(endIndex > numbers.Count - 1)
+                    {
+                        endIndex = numbers.Count - 1;
+                    }
+
+                    int endIndexToremove = endIndex - startIndex + 1;
+                    numbers.RemoveRange(startIndex, endIndexToremove);
+
+                    i = startIndex - 1;
+                }
+            }
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
