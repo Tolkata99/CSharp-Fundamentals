f6649ba5002de5793f05d1c6831b8fa8df12394a
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-20 10:48:19 +0300

    10.20.20

---
+++ cardGame/ApendAray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApendAray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            list.Reverse();
+
+            string[] reversArrayFromList = new string[list.Count];
+            foreach (var item in list)
+            {
+                int currentNumber = list[item];
+
+                reversArrayFromList[item] = list[item];
+            }
+            
+
+            Console.WriteLine(string.Join("", reversArrayFromList));
+                
+        }
+    }
+}
