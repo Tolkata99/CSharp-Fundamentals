0b3abbb34046ea430e1d28eab51787184ecb0a64
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-20 10:47:38 +0300

    20.10.20

---
+++ 10.20.20/10.20.20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApendAray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .Split("|")
+                .ToList();
+
+            list.Reverse();
+
+            List<string> result = new List<string>();
+            foreach (string currItems in list)
+            {
+                string[] numbers = currItems
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                foreach (var numbersToadd in numbers)
+                {
+                    result.Add(numbersToadd);
+                }
+                
+            }
+
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
