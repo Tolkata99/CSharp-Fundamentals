b5e6ce48d4ac30eb496193df417a3931dc3081c5
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-10-27 14:26:25 +0200

    10.27.20

---
+++ MethodAndObject/MethodAndObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Random list = new Random();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                int index = list.Next(0, words.Count);
+                string word = words[i];
+
+                words[i] = words[index];
+                words[index] = word;
+
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+            
+        }
+    }
+}
