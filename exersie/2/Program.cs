3eb5cffd53152f3cf5bc554c3eb80a023294e9cf
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-01 19:35:45 +0200

    01.11.2020

---
+++ exersie/2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> craft = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+            while (true)
+            {
+                List<string> comands = Console.ReadLine()
+                .Split("-")
+                .ToList();
+                string[] comand1 = comands[0].Split(":");
+
+                if(comand1 == "Craft!")
+                {
+                    break;
+                }
+                else if(comand1 == "Collect")
+                {
+                    bool isHave = craft.Contains(comands[1]);
+                    if(!isHave)
+                    {
+                        craft.Add(comands[1]);
+                    }
+
+                }
+                else if (comand1 == "Combine Items  ")
+                {
+                    bool isHave = craft.Contains(comands[1]);
+                    if(isHave)
+                    {
+
+                        craft.Insert(1, comands[2]);
+                    }
+                }
+                else if (comand1 == "Renew ")
+                {
+                    bool isHave = craft.Contains(comands[1]);
+                    if(isHave)
+                    {
+                        craft.Remove(comands[1]);
+                        craft.Add(comands[1]);
+
+                    }
+
+                }
+                else if (comand1 == "Drop")
+                {
+                    bool isHave = craft.Contains(comands[1]);
+                    if(isHave)
+                    {
+                        craft.Remove(comands[1]);
+                    }
+                }
+
+
+            }
+            Console.WriteLine(string.Join(" ",craft));
+        }
+    }
+}
---
+++ exersie/exersie/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace exersie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents = int.Parse(Console.ReadLine());
+            int numLesson = int.Parse(Console.ReadLine());
+            int firstBonus = int.Parse(Console.ReadLine());
+
+            double currLesson = 0 ;
+            double min = double.MinValue;
+
+            for (int i = 0; i < numStudents; i++)
+            {
+                double presence = double.Parse(Console.ReadLine());
+
+                double totalBonus = (presence / numLesson) * (5 + firstBonus);
+                
+                if(totalBonus > min)
+                {
+                    min = totalBonus;
+                    currLesson = presence;
+                }
+            }
+            Console.WriteLine($"Max Bonus: {Math.Round(min)}.");
+            Console.WriteLine($"The student has attended {currLesson} lectures.");
+
+        }
+    }
+}
