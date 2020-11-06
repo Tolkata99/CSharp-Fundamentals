3aa443c00921fa18ae6768aa47b2c48c680c78b6
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-06 18:26:36 +0200

    c

---
+++ NationalCourt/Numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+              .Split(" ")
+              .Select(int.Parse)
+              .ToList();
+            int[] topFive = new int[4];
+            int startIndx = 0;
+          
+            double average = numbers.Average();
+            while (true)
+            {
+                
+                if (average >= numbers[startIndx])
+                {
+                    
+                    numbers.RemoveAt(startIndx);
+                    
+                }
+                else
+                {
+                    if(numbers.Count >= startIndx)
+                    {
+                        startIndx++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                    
+                }
+            }
+
+            
+
+            //numbers.Sort();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
