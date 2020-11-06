b86e4a4f2e14e64243eb56532ba0710d0766e3ca
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-06 18:26:52 +0200

    c

---
+++ NationalCourt/NationalCourt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeesOneAsk = int.Parse(Console.ReadLine());
+            int employeesTwoAsk = int.Parse(Console.ReadLine());
+            int employeesThreeAsk = int.Parse(Console.ReadLine());
+
+            int peopleWaiting = int.Parse(Console.ReadLine());
+
+            int totalTimeEmployeesHour = employeesOneAsk + employeesThreeAsk + employeesTwoAsk;
+            int hours = 0;
+
+            while (peopleWaiting > 0)
+            {
+                hours++;
+                if(hours % 4 == 0)
+                {
+                    continue;
+                }
+
+                peopleWaiting -= totalTimeEmployeesHour;
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
