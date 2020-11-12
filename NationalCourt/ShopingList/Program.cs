5419c56c9b5663fb981912b9e5233c99f4f942bd
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-12 20:36:16 +0200

    s

---
+++ NationalCourt/ShopingList/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             List<string> listsFood = Console.ReadLine()
+                .Split("!")
+                .ToList();
+            string input = Console.ReadLine();
+            while (input != "Go Shopping!")
+            {
+                string[] tokens = input.Split(" "); 
+                string comandFirst = tokens[0];              
+                string comandSecond = tokens[1];
+
+                if (comandFirst == "Urgent")
+                {
+                    bool isHave = listsFood.Contains(comandSecond);
+                    if (!isHave)
+                    {
+                        listsFood.Insert(0, comandSecond);
+                    }
+                    
+                }
+                else if (comandFirst == "Unnecessary")
+                {
+                    bool isHave = listsFood.Contains(comandSecond);
+                    if (isHave)
+                    {
+                        listsFood.Remove(comandSecond);
+                    }
+                }
+                else if (comandFirst == "Correct")
+                {
+                    bool isHave = listsFood.Contains(comandSecond);
+                    
+                    if (isHave)
+                    {
+                        int index = listsFood.IndexOf(comandSecond);
+                        listsFood.Remove(comandSecond);
+                        listsFood.Insert(index, tokens[2]);
+                    }
+
+                    
+                }
+                else if (comandFirst == "Rearrange")
+                {
+                    bool isHave = listsFood.Contains(comandSecond);
+                    if(isHave)
+                    {
+                        listsFood.Remove(comandSecond);
+                        listsFood.Add(comandSecond);
+
+                    }
+                    
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ",listsFood));
+        }
+    }
+}
---
+++ NationalCourt/ValentineDay/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ValentineDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neightborhood = Console.ReadLine()
+                .Split("@")
+                .Select(int.Parse)
+                .ToArray();
+
+            int jumpPositions = 0;
+            string input = Console.ReadLine();
+
+            while (input != "Love!")
+            {
+                
+                string[] comands = input
+                     .Split(" ");
+                int jumpLenght = int.Parse(comands[1]);
+
+                jumpPositions += jumpLenght;
+                if(jumpPositions >= 0 && jumpPositions < neightborhood.Length)
+                {
+                    neightborhood[jumpPositions] -= 2;
+                }
+                else
+                {
+                    jumpPositions = 0;
+                    neightborhood[jumpPositions] -= 2;
+                }
+
+                if(neightborhood[jumpPositions] == 0)//vvgreshka
+                {
+                    Console.WriteLine($"Place {jumpPositions} has Valentine's day.");
+                }
+                else if(neightborhood[jumpPositions] < 0)
+                {
+                    Console.WriteLine($"Place {jumpPositions} already had Valentine's day.");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Cupid's last position was {jumpPositions}.");
+            
+            int faileCount = neightborhood.Count(x => x > 0);
+            if (faileCount == 0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {faileCount} places.");
+            }
+
+
+
+        }
+    }
+}
