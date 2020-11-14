fd38fd8bb133bc86d94109625327d9f69a8809c5
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-14 09:33:04 +0200

    lab

---
+++ lab/OneTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OneTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nmbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double,int>();
+
+            foreach (int number in nmbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number,1);
+                }
+            }
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+
+
+        }
+    }
+}
