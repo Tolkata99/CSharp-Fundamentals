afdf74b7e2ff2a074b863ae592d6ed19b6d2e32e
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-14 09:32:15 +0200

    lab

---
+++ lab/Odd Words/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odd_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordToLower = word.ToLower();
+                if(counts.ContainsKey(wordToLower))
+                {
+                    counts[wordToLower]++;
+                }
+                else
+                {
+                    counts.Add(wordToLower, 0);
+                }
+
+            }
+
+            List<string> printWords = new List<string>();
+
+            foreach (var item in counts)
+            {
+                if(item.Value % 2 == 0)
+                {
+                   
+                    printWords.Add(item.Key);
+                }
+               
+                
+            }
+
+            Console.WriteLine(string.Join(" ", printWords));
+
+
+        }
+    }
+}
