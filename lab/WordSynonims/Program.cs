8ef12186d3b38e3d6d295367d76e70286946f388
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-14 09:33:31 +0200

    lab

---
+++ lab/WordSynonims/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordSynonims
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var words = new Dictionary<string, List<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonims = Console.ReadLine();
+                
+                if(words.ContainsKey(word) == false)
+                {
+                    words.Add(word, new List<string>());
+                }
+                words[word].Add(synonims);
+            }
+
+            foreach (var item in words)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
