1155b1b6539eed9fa9db74ad60b166d2bcf7ffac
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-14 09:32:00 +0200

    lab

---
+++ lab/LINQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .ToArray();
+
+            int count = 0;
+
+            if(numbers.Length <= 3)
+            {
+                count = numbers.Length;
+            }
+            else
+            {
+                count = 3;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+
+            
+        }
+    }
+}
