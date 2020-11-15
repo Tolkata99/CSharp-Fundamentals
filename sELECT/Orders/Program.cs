82d3f0e8e15f698cdfba665742b44e6ff477c5cf
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-15 17:26:02 +0200

    d

---
+++ sELECT/Orders/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> productsAndPrices = new Dictionary<string, List<double>>();
+
+           while (true)
+           {
+               string[] input = Console.ReadLine().Split().ToArray();
+               string product = input[0];
+               if (product == "buy")
+               {
+                   break;
+               }
+               double price = double.Parse(input[1]);
+               int number = int.Parse(input[2]);
+
+                List<double> totalPrice = new List<double> { price, number };
+               if (productsAndPrices.ContainsKey(product) == false)
+               {
+                    
+                   productsAndPrices.Add(product, totalPrice);
+               }
+               else
+               {
+                   productsAndPrices[product][0] = price;
+                    productsAndPrices[product][1] += number;
+                }
+           
+           
+           
+           
+           
+           
+           }
+           foreach (var item in productsAndPrices)
+           {
+               string product = item.Key;
+               double totPrice = item.Value[0] * item.Value[1];
+           
+               Console.WriteLine($"{product} -> {totPrice:f2}");
+           }
+        }  
+    }      
+}          
