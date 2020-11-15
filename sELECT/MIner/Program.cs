d6450fde847ae048f6ce1151dd7085b6ecea8877
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-15 17:28:14 +0200

    FindGold

---
+++ sELECT/MIner/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MIner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string resoures = Console.ReadLine();
+                if(resoures == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (result.ContainsKey(resoures) == false)
+                {
+                    result.Add(resoures, quantity);
+                }
+                else
+                {
+                    result[resoures] += quantity;
+                }
+                
+
+
+
+
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
