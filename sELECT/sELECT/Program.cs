0ad84a9841aad4311db52c5b743c7d632a3fc84e
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-15 17:24:57 +0200

    d

---
+++ sELECT/sELECT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace sELECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Select(w => w + "2wweaz")
+                .ToArray();
+            Console.WriteLine(string.Join("' ",words));
+        }
+    }
+}
