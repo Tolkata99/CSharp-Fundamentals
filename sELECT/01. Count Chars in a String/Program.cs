56eecac61372c56bf10434aa40191f7ac8def5c0
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-18 09:03:18 +0200

    Count chars in a String

---
+++ sELECT/01. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> ocurrences = new Dictionary<char, int>();
+
+            foreach (char letter in input)
+            {
+                if(letter != ' ')
+                {
+                    if(ocurrences.ContainsKey(letter) == false)
+                    {
+                        ocurrences.Add(letter, 0);
+                    }
+                    ocurrences[letter]++;
+                }
+            }
+
+            foreach (var item in ocurrences)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
