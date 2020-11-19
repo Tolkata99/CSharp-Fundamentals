f14ea957166e502c591cebffd13a0cba6421a46e
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-19 20:39:58 +0200

    I solved problem 04 from my lab.

---
+++ Company Users/SubString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string removedString = Console.ReadLine().ToLower();
+
+            string text = Console.ReadLine();
+            int index = text.IndexOf(removedString);
+
+            while (index != -1)
+            {
+                text = text.Remove(index, removedString.Length);
+
+                index = text.IndexOf(removedString);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
