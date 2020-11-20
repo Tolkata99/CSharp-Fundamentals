10487028371f3df4631b2f358f366bd6bbf5d766
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-20 11:20:27 +0200

    solved task from Soft

---
+++ Company Users/TextFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] replace = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string text = Console.ReadLine();
+                
+
+            for (int i = 0; i < replace.Length; i++)
+            {
+                text = text.Replace(replace[i], new string('*', replace[i].Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
