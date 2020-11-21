f7c7da189e14f4f31b3fe4e2f4a2957a4cf90b8a
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-21 13:36:42 +0200

    I solved CharArray in Exersize!

---
+++ 07CharArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _07CharArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            var input = Console.ReadLine().ToCharArray();
+            string result = string.Empty;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                
+                if(input[i] != input[i+1])
+                {
+                    sb.Append(input[i]);
+                }
+            }
+            sb.Append(input[input.Length - 1]);
+            Console.WriteLine(sb);
+        }
+    }
+}
