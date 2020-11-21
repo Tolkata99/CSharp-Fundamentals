73af995e7821aac3be96ab3a868764ce1126eb94
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-21 13:37:27 +0200

    I solved ValidUsername in Exersize!

---
+++ 01ValidUsernames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var input = Console.ReadLine().Split(", ");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var cur = input[i];
+               if( isValid(cur))
+                {
+                    Console.WriteLine(cur);
+                }
+            }
+           
+
+
+           
+            
+        }
+        public static bool isValid(string current)
+        {
+            return current.Length >= 3 &&
+                current.Length <= 16 &&
+                current.All(c => char.IsLetterOrDigit(c)) ||
+                current.Contains("-") || current.Contains("_");
+                
+
+        }
+    }
+}
