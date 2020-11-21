39bcf8164e7bc4ee07de95e372196788cfb45b8d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-21 13:37:12 +0200

    I solved Crypto in Exersize!

---
+++ 04Crypto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string crypto = string.Empty;
+            foreach (char ch in text)
+            {
+                var currChar = (char)(ch + 3);
+                crypto += currChar;
+            }
+            Console.WriteLine(crypto);
+        }
+    }
+}
