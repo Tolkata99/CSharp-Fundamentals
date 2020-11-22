d954a215a67382d7bbcbaa6f72a424dda95dbc0d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-22 18:32:21 +0200

    I solved CharacterMultiply from exercise !

---
+++ 02CharacterMultypl/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02CharacterMultypl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstWord = input[0];
+            string secondWord = input[1];
+
+
+            var longestWord = firstWord;
+            var shortersWord = secondWord;
+            if (firstWord.Length < shortersWord.Length)
+            {
+                longestWord = secondWord;
+                shortersWord = firstWord;
+            }
+
+            Console.WriteLine(CharMultiply(longestWord, shortersWord));
+        }
+        public static int CharMultiply(string longestWord, string shortersWord)
+        {
+            int result = 0;
+            for (int i = 0; i < shortersWord.Length; i++)
+            {
+
+
+                result += shortersWord[i] * longestWord[i];
+
+            }
+            for (int i = shortersWord.Length; i < longestWord.Length; i++)
+            {
+                result += longestWord[i];
+            }
+
+            return result;
+        }
+    }
+}
