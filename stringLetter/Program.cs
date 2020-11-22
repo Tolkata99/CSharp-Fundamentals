cd22e55f6aa992eb11e15d15dbe492e423425ed2
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-22 18:33:10 +0200

    I solved StringLetter from exercise !

---
+++ stringLetter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace stringLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = (Console.ReadLine());
+            string letters = string.Empty;
+            string numbers = string.Empty;
+            string others = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    letters += text[i];
+                }
+                else if (char.IsDigit(text[i]))
+                {
+                    numbers += text[i];
+                }
+                else
+                {
+                    others += text[i];
+                }
+
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
