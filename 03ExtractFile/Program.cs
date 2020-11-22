d0c59f354bae4995861120d3fbe7919ed2535814
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-22 18:32:00 +0200

    I solved ExtractFile from exercise !

---
+++ 03ExtractFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+            int length = input.Length;
+            string file = input[length - 1];
+
+            string[] files = file.Split(".");
+
+            string first = files[0];
+            string seconds = files[1];
+
+
+            Console.WriteLine($"File name: {first}");
+            Console.WriteLine($"File extension: {seconds}");
+        }
+    }
+}
