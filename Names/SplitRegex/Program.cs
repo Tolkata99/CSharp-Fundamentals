d59bdb226d7307db70b6a2d5ef5ca8fd2d1e0f4d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-29 11:01:12 +0200

    11.29.2020

---
+++ Names/SplitRegex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SplitRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "1   2  3      0";
+            string patern = @"\s+";
+            
+
+
+            string[] result = Regex.Split(text, patern);
+            Console.WriteLine(string.Join(" ",result));
+
+        }
+    }
+}
