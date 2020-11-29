34de2e64c53e1f67004a9fba078918a7f38a91be
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-11-29 11:02:12 +0200

    i solved exerciseRegex

---
+++ Names/ExersizeRegex/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExersizeRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(\w[A-Za-z]+)<<(\d[(0.00-9.00|0-9)*]+)!(\d+)";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+
+
+            double total = 0.00;
+           Console.WriteLine("Bought furniture:");
+            while (text != "Purchase")
+            {
+                Match match = regex.Match(text);
+                if(match.Success)
+                {
+                    string printName = match.Groups[1].Value;
+                    Console.WriteLine(printName);
+
+                    double result = double.Parse(match.Groups[2].Value) * double.Parse(match.Groups[3].Value);
+                    total += result;
+                }
+                
+
+
+
+
+                text = Console.ReadLine();
+                
+            }
+            Console.WriteLine($"Total money spend: {total:f2}");
+            
+            
+            
+        }
+    }
+}
