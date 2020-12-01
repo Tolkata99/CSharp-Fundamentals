445f9f4e1ca5ad9b0808b249b52a1a5ca0133052
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-01 21:12:31 +0200

    I solved TextProcesingMoreExercise

---
+++ TextProcesingMoreExercise/TextProcesingMoreExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TextProcesingMoreExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            2
+            //Here is a name @George | and an age #18*
+            //Another name @Billy | #35* is his age
+
+            int n = int.Parse(Console.ReadLine());
+
+            string text = Console.ReadLine();
+            string pattern = @"@(\w+)";
+            string patternAge = @"#(\d+)";
+
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                Match matchName = Regex.Match(text,pattern);
+                Match matchAge = Regex.Match(text,patternAge);
+
+                
+
+
+
+
+                Console.WriteLine($"{matchName.Groups[1].Value} is {matchAge.Groups[1].Value} years old.");
+
+
+                text = Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
