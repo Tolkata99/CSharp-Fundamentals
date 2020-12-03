30e47e559d5236144408610910fb5ac3ec7e464d
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-03 19:15:43 +0200

    I solved char

---
+++ char/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace chara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            int result = 0;
+            string text = Console.ReadLine();
+
+
+            foreach (char item in text)
+            {
+                if (a < item && b > item)
+                {
+                    result += item;
+                }
+            }
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
