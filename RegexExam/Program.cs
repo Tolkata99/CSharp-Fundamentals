483df174cf99e4d879afd02f233dcbc0205cce90
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-03 19:16:32 +0200

    I solved task with regex

---
+++ RegexExam/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegexExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(\d)";
+            string patternName = @"([:]{2}|[*]{2})(?<name>[A-Z][a-z]{2,})\1";
+
+            List<String> names = new List<string>();
+
+
+            MatchCollection matchCol = Regex.Matches(text, pattern);
+            double coldNumber = 1;
+
+            for (int i = 0; i < matchCol.Count; i++)
+            {
+               coldNumber *= int.Parse(matchCol[i].Value);
+            }
+            Console.WriteLine($"Cool threshold: {coldNumber}");
+
+            
+
+
+            MatchCollection name = Regex.Matches(text, patternName);
+            
+            for (int i = 0; i < name.Count; i++)
+            {
+                double sumName = 0;
+                foreach (char item in name[i].Groups[2].Value)
+                {
+                    
+                    sumName += item;
+                }
+                if(coldNumber < sumName)
+                {
+                    
+                    names.Add(name[i].Groups[0].Value);
+                }
+               
+            }
+            Console.WriteLine($"{name.Count} emojis found in the text. The cool ones are:");
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
