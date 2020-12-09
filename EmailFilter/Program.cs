509400e83ebdfbc0243a8caa9ef8d8299dc3d0d4
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-09 22:21:21 +0200

    I solved EmailFilter

---
+++ EmailFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\S(\w+\.?\w*)[^.-_\/]\S(\w+\.?\w*)@(\w+)(\w+\.?\w*)(\w+\.?\w)";
+
+            Regex regex = new Regex(pattern);
+            string text = Console.ReadLine();
+
+            MatchCollection match = regex.Matches(text);
+
+            foreach (var item in match)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
