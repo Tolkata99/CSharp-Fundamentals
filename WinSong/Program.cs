6c3f6fd8d1e9cbf804093ef61230ea64f0df1ebd
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-19 13:44:08 +0200

    I solved winSONGS

---
+++ WinSong/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinSong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numSong = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numSong; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split("_", StringSplitOptions.RemoveEmptyEntries);
+                string typeList = tokens[0];
+                string name = tokens[1];
+                string time = tokens[2];
+
+                Song song = new Song();
+
+                song.TypeList = typeList;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+
+
+
+            }
+
+            string comand = Console.ReadLine();
+            if(comand == "All")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if(comand == song.TypeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+    class Song
+    {
+        public string TypeList { get; set; }
+
+        public string  Name { get; set; }
+
+        public string  Time { get; set; }
+    }
+}
