f41c8a300a3959394a0dbe6d0dfec6b7dde6e0ab
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2020-12-20 10:41:01 +0200

    solved

---
+++ EmailFind/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})[-.\/](?<month>\w{3})[-.\/](?<year>\d{4})\b";
+
+            Regex regex = new Regex(pattern);
+            string text = Console.ReadLine();
+
+            MatchCollection matchCol = regex.Matches(text);
+
+            foreach (Match item in matchCol)
+            {
+                Console.WriteLine($"{item.Groups["day"].Value}{item.Groups["month"].Value}{item.Groups["year"].Value}"); 
+            }
+
+        }
+    }
+}
